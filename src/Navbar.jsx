3f749af500f908abb2c7b2e25261b5ac6d48f622
Shana Adams <shana.adams10@gmail.com>
2024-06-17 17:08:11 -0600

    Add Navbar component with routes.

---
+++ src/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">About</Link>
+        </li>
+        <li>
+          <Link to="/resume">Resume</Link>
+        </li>
+        <li>
+          <Link to="/works">Works</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
