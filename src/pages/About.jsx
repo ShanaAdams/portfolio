3660d02fb8032cbd0917830a91d616f13b8928bf
Shana Adams <shana.adams10@gmail.com>
2024-06-18 07:10:35 -0600

    Add description of work and experience on About me.

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -8,12 +8,27 @@ const About = () => {
         flexDirection: "column",
         justifyContent: "center",
         alignItems: "center",
-        width: "50%",
+        width: "100%",
         margin: "50px auto 20px",
       }}
     >
-      <h1>About</h1>
-      <p>This is the About page.</p>
+      <h1>About Me</h1>
+      <h2>
+        <i>Full Stack Developer with Marketing, SEO Expertise</i>
+      </h2>
+      <p>
+        Six months of fullstack development experience working on agile teams.
+        Including a web application for a startup in Los Angeles. An ability to
+        work on multiple projects simultaneously. Expertise in marketing with
+        over 7 years of management experience and over 13 years of exceptional
+        sales and customer service experience. Including working with
+        senior-level managers on marketing projects and over 6 years of
+        marketing experience. Two years of experience with brand audits
+        including digital marketing strategy, content audits, and strategic
+        planning. Well known for having superior communication skills, strategic
+        planning, crisis management, a demonstrated commitment to customer
+        service, and as an accomplished writer with copywriting experience.
+      </p>
     </div>
   );
 };
