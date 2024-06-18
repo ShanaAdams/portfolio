a11e5acea1e9647db7b06481b08a6ddf03bda1eb
Shana Adams <shana.adams10@gmail.com>
2024-06-18 06:51:00 -0600

    Add div styling to About, Resume, & Works page so contents are visable and not under the navbar.

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,16 @@ import React from "react";
 
 const About = () => {
   return (
-    <div>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "50%",
+        margin: "50px auto 20px",
+      }}
+    >
       <h1>About</h1>
       <p>This is the About page.</p>
     </div>
--- src/pages/Works.jsx
+++ src/pages/Works.jsx
@@ -2,7 +2,16 @@ import React from "react";
 
 const Works = () => {
   return (
-    <div>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "50%",
+        margin: "50px auto 20px",
+      }}
+    >
       <h1>Works</h1>
       <p>This is the Works page.</p>
     </div>
